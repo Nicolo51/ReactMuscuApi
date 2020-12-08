6b92a449c9680f81b7edea753bc57a35244a5b4a
Nicolas Descotes <nicolas.descotes@viacesi.fr>
2020-12-08 16:08:26 +0100

    update log message

--- Modules_pi/Log.js
+++ Modules_pi/Log.js
@@ -1,4 +1,4 @@
 exports.log = function(msg){
-    var date = "|[" + new Date().getFullYear().toString() + "-" + new Date().getMonth().toString() + "-" + new Date().getDay().toString() + " " + new Date().getHours().toString() + ":" + new Date().getMinutes().toString() + ":" + new Date().getSeconds().toString()+ "]"; 
+    var date = "[" + new Date().getFullYear().toString() + "-" + new Date().getMonth().toString() + "-" + new Date().getDay().toString() + " " + new Date().getHours().toString() + ":" + new Date().getMinutes().toString() + ":" + new Date().getSeconds().toString()+ "]"; 
     console.log(date , msg);
 }
